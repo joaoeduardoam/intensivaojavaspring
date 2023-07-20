bfb4526b4e131713bb799e7d0f72477452eb86bf
João Eduardo Meneses <jeduado.meneses@gmail.com>
2023-07-20 16:30:57 -0300

    Homolog with postgres db ok

--- src/main/java/com/devsuperior/dslist/dto/GameMinDTO.java
+++ src/main/java/com/devsuperior/dslist/dto/GameMinDTO.java
@@ -16,6 +16,6 @@ public record GameMinDTO(
     }
 
     public GameMinDTO (GameMinProjection projection){
-        this (projection.getId(), projection.getTitle(), projection.getYear(), projection.getImgUrl(), projection.getShortDescription());
+        this (projection.getId(), projection.getTitle(), projection.getGameYear(), projection.getImgUrl(), projection.getShortDescription());
     }
 }
